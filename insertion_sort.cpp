9af90c6ab9920a9cd8d3908bcbb358ddbf583804
PINYALAI <alfurqonikazd887@gmail.com>
2026-03-12 11:25:22 +0700

    void display

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -49,3 +49,14 @@ void insertionsort(){ //procedure insertion sort
 }
 }
 
+void display (){
+    cout << "\n======================";
+    cout << "nElement Array" << endl;
+    cout << "========================" << endl;
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
